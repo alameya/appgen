5635e53b012b1f32a3595684a20339de53c62f16
Raul Yagudin <yaraul@gmail.com>
2025-02-20 21:18:24 +0300

    Update generated code: 2025-02-20 21:18:24

--- dev/generate.sh
+++ dev/generate.sh
@@ -14,35 +14,6 @@ mkdir -p out/internal/grpc
 mkdir -p out/internal/proto
 mkdir -p out/migrations
 
-# Determine which MD5 command to use
-if command -v md5sum >/dev/null 2>&1; then
-    MD5_CMD="md5sum"
-elif command -v md5 >/dev/null 2>&1; then
-    MD5_CMD="md5 -r"
-else
-    echo "Neither md5sum nor md5 command found"
-    exit 1
-fi
-
-# Calculate MD5 sum of all proto files
-PROTO_MD5=$(find proto -name "*.proto" -type f -exec $MD5_CMD {} \; | sort | $MD5_CMD | cut -d' ' -f1)
-MD5_FILE=".proto.md5"
-
-# Check if MD5 has changed
-NEED_GENERATE=1
-if [ -f "$MD5_FILE" ]; then
-    OLD_MD5=$(cat "$MD5_FILE")
-    if [ "$OLD_MD5" == "$PROTO_MD5" ]; then
-        NEED_GENERATE=0
-        echo "Proto files have not changed, skipping code generation..."
-    fi
-fi
-
-echo $NEED_GENERATE
-
-# Save current MD5
-echo "$PROTO_MD5" > "$MD5_FILE"
-
 # Generate proto files
 echo "Generating proto files..."
 
@@ -87,8 +58,6 @@ mkdir -p out/internal/proto
 mv out/app/internal/proto/* out/internal/proto/
 rm -rf out/app
 
-
-
 # Format generated code
 echo "Formatting generated code..."
 find out -name "*.go" -exec go fmt {} \;
@@ -127,11 +96,6 @@ SERVICE_PID=$!
 
 # Trap Ctrl+C and kill the service
 trap 'echo "Stopping service..."; kill $SERVICE_PID; exit' INT
-
-# Wait for service to finish (it won't unless interrupted)
-wait $SERVICE_PID
-
-echo "Done! You can now run the server with: cd out && go run cmd/app/main.go" 
 
 # Wait for service to finish (it won't unless interrupted)
 wait $SERVICE_PID
